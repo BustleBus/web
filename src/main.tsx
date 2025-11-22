4111fdc20e4e6d8f28b912c6eb1d3d84480a28f1
hojin535 <hojin535@naver.com>
2025-11-22 10:21:39 +0900

    feat: 구버전 xlsx,csv 파일 삭제 및 인식 코드 삭제, 파일정리

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import ExperimentPage from './components/ExperimentPage.tsx';
 
-const root = createRoot(document.getElementById('root')!);
-
-const isTestMode = new URLSearchParams(window.location.search).get('test') === 'chart';
-
-root.render(
-  <StrictMode>
-    {isTestMode ? <ExperimentPage /> : <App />}
-  </StrictMode>,
+createRoot(document.getElementById('root')!).render(
+    <App />
 )
